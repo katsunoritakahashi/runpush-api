28e9574453c1dccaaf080b7401324bab7794d001
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-07-23 17:25:05 +0900

    [fix]テーブル作成

--- src/app/database/migrations/2022_06_02_000000_create_users_table.php
+++ src/app/database/migrations/2022_06_02_000000_create_users_table.php
@@ -15,17 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('family_id')->nullable()->index('fk_user_family1_idx')->comment('家族ID');
-            $table->foreign('family_id', 'fk_user_family1')->references('id')->on('families')->onUpdate('NO ACTION')->onDelete('CASCADE');
-            $table->string('name')->comment('名前');
-            $table->unsignedInteger('position_id')->nullable()->index('fk_user_position1_idx')->comment('立場ID');
-            $table->foreign('position_id', 'fk_user_position1')->references('id')->on('positions')->onUpdate('NO ACTION')->onDelete('CASCADE');
-            $table->string('email')->unique()->comment('メールアドレス');
-            $table->timestamp('email_verified_at')->nullable()->comment('メールアドレス確認日時');
-            $table->string('password')->comment('パスワード');
-            $table->string('icon_url', 200)->nullable()->comment('iconURL');
-            $table->string('device_token', 200)->nullable()->comment('デバイストークン');
-            $table->rememberToken();
+            $table->string('uid')->comment('firebaseのuid');
+            $table->string('device_token', 200)->comment('デバイストークン');
+            $table->tinyInteger('character_id')->comment('キャラクターID');
+            $table->dateTime('end_at')->comment('終了日時');
             $table->timestamps();
         });
     }

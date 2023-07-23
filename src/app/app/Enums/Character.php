b7d180eda0bdabb8d06ef01279c5c299429a8749
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-07-23 17:31:50 +0900

    [fix]routerの設定

---
+++ src/app/app/Enums/Character.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static RUNTEKUN()
+ * @method static static HISAJU()
+ * @method static static PHARAOH()
+ */
+final class Character extends Enum
+{
+    const RUNTEKUN = 1;
+    const HISAJU = 2;
+    const PHARAOH = 3;
+}

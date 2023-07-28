74e2126462229c468650bdd07bc599f123e98386
Takahashi Katsunori <katsutennis.916@gmail.com>
2023-07-28 10:43:29 +0900

    [fix]メッセージ追加

--- src/app/database/seeders/CharacterMessagesSeeder.php
+++ src/app/database/seeders/CharacterMessagesSeeder.php
@@ -10,7 +10,12 @@ class CharacterMessagesSeeder extends Seeder
 {
     private $messages = [
         [Character::RANTEKUN, 'カリキュラムの調子はどうかな？無理なく頑張ってくれよな！応援してるぞ！'],
+        [Character::RANTEKUN, 'エンジニア転職を目指してる姿、とても素敵だぞ！着実に夢に近づいてるから無理せず頑張ってくれよな！'],
+        [Character::RANTEKUN, 'カリキュラムで困ったことがあったら気軽に友達の「ロボカエルとキツネのキメラくん」に相談してみてくれよな！'],
         [Character::HISAJU, '仕事(twitter)ちゃんとしましょうー'],
+        [Character::HISAJU, 'Vimちゃんと使えてますか？（圧）'],
+        [Character::HISAJU, 'アモアスやりすぎると禁止にしますよー'],
+        [Character::HISAJU, 'カリキュラムでわからないことあれば、是非「ロボカエルとキツネのキメラくん」に質問してみてくださいー'],
         [Character::PHARAOH, '𓎡𓍯𓅓𓄿𓏏𓏏𓄿𓎡𓍯𓏏𓍯𓎼𓄿𓄿𓂋𓇌𓃀𓄿𓈖𓄿𓈖𓂧𓇌𓅓𓍯𓎡𓇋𓇋𓏏𓇌𓈖𓇌'],
     ];
 
